8f3a639d99b4e425574d048139d947264fc98ec5
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2020-08-17 12:34:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ceph ePub Builder
+
+This patches the ceph repository and lets you build the entire Ceph documentation as an .epub for easy consumption and reference.
+
+### Usage:
+* `./build.sh [git tag/commit]`
+* You might get asked to install dependencies by the documentation builder if you don't have them.
+* Result is copied to root directory
---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "${DIR}"
+
+CEPH_TAG="${1:-origin/master}"
+
+git submodule update --init
+
+pushd ceph
+
+rm -rf build-doc
+
+git fetch --all
+
+git am --abort
+git clean --force -x
+git reset --hard "${CEPH_TAG}"
+git clean --force -x
+
+git am --ignore-space-change --3way < ../patches/0001-Added-EPUB-document-generation.patch
+git am --ignore-space-change --3way < ../patches/0002-Added-conf.py-changes-for-EPUB-generation.patch
+
+#export CEPH_THEME="ceph"
+export CEPH_THEME="epub"
+./admin/build-doc epub
+
+cp -vf "build-doc/output/epub/"*.epub ../
+
+popd
